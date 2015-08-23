fa4ce76283f868121bd632175d3ef9fbef89fccc
Nathan Ockerman <ockerman.ns@gmail.com>
2015-08-22 23:36:58 -0700

    Player tokens sized and placed. With initial object placing functions complete.

--- Dot./app.js
+++ Dot./app.js
@@ -1,5 +1,61 @@
 // OOP Racing Game example boilerplate code
 
+
+/*$(document).ready(function(){
+
+});*/
+
+
+
+
+/*
+  //works!!
+function setPlayer(player){
+
+    var playerOneTemplate = _.template($('#playerOne-template').html());
+    var playerTwoTemplate = _.template($('#playerTwo-template').html());
+  
+    if (player === playerOne) {
+      var playerOneHtml = playerOneTemplate(player);
+      $("#playerOne-placeholder").append(playerOneHtml);
+    } else if (player === playerTwo) {
+      var playerTwoHtml = playerTwoTemplate(player);
+      $("#playerTwo-placeholder").append(playerTwoHtml);
+    } 
+  };
+*/
+
+
+
+function Player() {}/*function(name, color) {
+  this.name = name,
+  this.color = color, //set color with a CSS function that set background color to token div
+};*/
+
+var setOne = 1;
+var setTwo = 2;
+Player.prototype = {
+  position: function(player){
+              console.log("Set player");
+              var player = player;
+              console.log(player);
+              var playerOneTemplate = _.template($('#playerOne-template').html());
+              var playerTwoTemplate = _.template($('#playerTwo-template').html());
+            
+              if (player === 1) {
+                var playerOneHtml = playerOneTemplate(player);
+                $("#playerOne-placeholder").append(playerOneHtml);
+              } else if (player === 2) {
+                var playerTwoHtml = playerTwoTemplate(player);
+                $("#playerTwo-placeholder").append(playerTwoHtml);
+              }
+            },
+  //next
+  //next
+  //next
+  //next
+};
+
 function Game() {
   //Create a new instance of player 1
   //this.player1 = ...
@@ -11,21 +67,43 @@ function Game() {
   //this.track = ...
 }
 
-// `Game.prototype.init` kicks off a new game with a board and two players
+// `Game.prototype.init` kicks off a new game 
+//    with a board and two players
 Game.prototype.init = function() {
   //
 };
 
 // A starter Player constructor.
-function Player(team) {
-  //this.name = ...
-  //this.position = ...
-};
+/*function Player(name, color ) {
+  this.name = name,
+  this.color = color,
+  //this.position = ...,
 
-// Remember: prototypes are shared functions between all game instances
-Player.prototype.move = function() {
+};*/
+
+
+
+/////////////////////test player
+/*var playerOne = {
+  name : "Nathan",
+  color : "red",
+  //token: <div></div>
+  //setPosition = 
+}
+
+var playerTwo = {
+  name: "Cheyenne",
+  color: "pink",
+}*/
+
+
+
+
+// Remember: prototypes are shared functions 
+//   between all game instances
+/*Player.prototype.move = function() {
   //update player's position
-};
+};*/
 
 
 // A starter Track constructor.
@@ -33,12 +111,43 @@ function Track() {
   //Tracks the cells of the board instance
   //this.$cells = ...
 
-  //Store any other properties that board may have below, such as a reset option
+  //Store any other properties that board may 
+  //   have below, such as a reset option
 };
 
 // Start the game!
-var game = new Game();
-game.init();
+/*var game = new Game();
+game.init();*/
+
+/*
+var playerOneTemplate = _.template($('#playerOne-template').html());
+
+var playerTwoTemplate = _.template($('#playerTwo-template').html());
+ */
+
+/*  //working set P1 function
+function setPlayer(player){
+   var playerOneHtml = playerOneTemplate(player);
+    $("#playerOne-placeholder").append(playerOneHtml);
+  };
+*/
+
+/*
+function setPlayer(player){
+  if (player === playerOne) {
+
+    var playerOneHtml = playerOneTemplate(player);
+    $("#playerOne-placeholder").append(playerOneHtml);
+
+  } else if (player === playerTwo) {
+
+    var playerTwoHtml = playerTwoTemplate(player);
+    $("#playerTwo-placeholder").append(playerTwoHtml);
+
+  } 
+};
+
+*/
 
 /*
 ///////////////////////////////////////////////
@@ -59,18 +168,100 @@ game.init();
 ///////////////////////////////////////////////////
 
 */
+/*
+$(function(){
+  
+  var playerOneTemplate = _.template($('#playerOne-template').html());
+  
+  //var octocats = data.octocats;
+ 
+  function setPlayer(player){
+   
+    var playerOneHtml = playerOneTemplate(player);
+    
+    $("#playerOne-placeholder").append(playerOneHtml);
+  };
 
 
 
 
+});*/
+
+/*
+$(function(){
+  //create our octocat template
+  var octocatTemplate = _.template($('#octocat-template').html());
+  // data is defined in data.js
+  var octocats = data.octocats;
+  // get our template to give us an html string for each octocat
+  octocats.forEach(function(octocat){
+    // give our template some data and get html from it
+    var octocatHtml = octocatTemplate(octocat);
+    // append resulting html to the placeholder area
+    $("#octocats-placeholder").append(octocatHtml);
+  });
+});*/
+
+
+//$('#playerOne-placeholder').html("<div class='playerOne'><p class='playerOne'>" + playerOne.name + "</p></div>");
+
+
+
+/*set name - underscore
+set color - $().css
+set position - l*/
+
+// A starter Player constructor.
+/*
+
+
+function Player(name, color ) {
+  this.name = name,
+  this.color = color, //set color with a CSS function that set background color to token div
+  
+
+};
+
+Player.prototype = {
+  position: function () {
+    $('
+  },
+}
+
+
+#playerOne-placeholder').html("<div class='playerOne'><p class='playerOne'>" + playerOne.name + "</p></div>");
 
 
 
 
+////////////////////////////////////////
+function Shoe() = {
+    this.laces = true;
+    this.color = “blue”; 
+}
 
 
+Shoe.prototype = {
+   tie : function () {
+       console.log(“i’m tying my shoes”);
+   },
+  untie: function(){
+       console.log(“I just untied my shoes”);
+   }
+} (edited)
 
 
+these are two separate entities: the Object definition and the Object prototype
+
+and yet you can still do
+
+
+var nike = new Shoe();
+
+
+nike.tie(); (edited)
+
+*/
 
 
 
--- Dot./intro.js
+++ Dot./intro.js
@@ -16,6 +16,12 @@
 ///////////////////////////////////////////////////////////////////////////
 
 /////////// Notes
+
+- have an initial pop up that asks one player or two
+- if one player set one input feild, if two set two inputs (underscore)
+- have function to gather info from input and set it to value
+- have ability to set color
+
  
 
 
@@ -28,3 +34,4 @@
 //////////////////////////////////////////////////////////////////////////
 */
 
+//  Okay, so I’m trying to set a position for the player at the start of the race in my Player one constructor and i’m at my wits end. Is there a way to set the object (when created) inside of a specific HTML element? Is it even possible? (I’ve been poking around google with no luck). Also having trouble figuring out how to set the position of two players with only one player constructor.
